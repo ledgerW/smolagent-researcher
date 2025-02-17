8833dd0673c66a67c710fdbe93dde9d7afa94450
Ledger West <ledger.west@gmail.com>
2025-02-17 14:15:41 +0000

    feat: api pydantic models basic

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import os
 import requests
-from typing import Union, Dict
-
+from typing import Union, Dict, Annotated
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 from agent import manager_agent
 
 
-app = FastAPI()
+class TaskInput(BaseModel):
+    task: Annotated[str, ..., "The task to be performed"]
+
+class TaskOutput(BaseModel):
+    output: Annotated[str, ..., "The result of the task"]
+
+
+app = FastAPI(title='Research Agents', version='0.1.0')
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/ai_policy_agent")
-def ai_policy_agent(task: str) -> Dict[str, str]:
-    return manager_agent.run(task)
+@app.post("/ai_policy_agent", response_model=TaskOutput)
+def ai_policy_agent(input: TaskInput) -> TaskOutput:
+    return TaskOutput(output=manager_agent.run(input.task))
 
     #requests.post(
     #    os.getenv("MAKE_AI_POLICY_HOOK"),
@@ -25,9 +32,9 @@ def ai_policy_agent(task: str) -> Dict[str, str]:
     #return {"result": res}
 
 
-@app.post("/ai_technical_agent")
-def ai_technical_agent(task: str) -> Dict[str, str]:
-    return manager_agent.run(task)
+@app.post("/ai_technical_agent", response_model=TaskOutput)
+def ai_technical_agent(input: TaskInput) -> TaskOutput:
+    return TaskOutput(output=manager_agent.run(input.task))
 
     #requests.post(
     #    os.getenv("MAKE_AI_TECHNICAL_HOOK"),
@@ -36,9 +43,9 @@ def ai_technical_agent(task: str) -> Dict[str, str]:
     #return {"result": res}
 
 
-@app.post("/global_macroeconomic_agent")
-def global_macroeconomic_agent(task: str) -> Dict[str, str]:
-    return manager_agent.run(task)
+@app.post("/global_macroeconomic_agent", response_model=TaskOutput)
+def global_macroeconomic_agent(input: TaskInput) -> TaskOutput:
+    return TaskOutput(output=manager_agent.run(input.task))
 
     #requests.post(
     #    os.getenv("MAKE_GLOBAL_MACROECONOMIC_HOOK"),
